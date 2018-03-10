d51210e7b9bed33318beed43108f6e8f6a0a71c0
Xavier <xleboss@msn.com>
2018-03-10 18:13:24 +0100

    logs + notification

---
+++ MyChat.Service/NotificationManager/INotificationManager.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IConnectionManager.cs">
+//    Copyright (c) 2018. All Rights reserved.
+// </copyright>
+// <summary>
+//    This interface defines the client connection manager.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MyChat.Service.ConnectionManager
+{
+    using MyChat.Service.Model;
+
+    /// <summary>
+    /// This interface defines the client connection manager.
+    /// </summary>
+    internal interface IConnectionManager
+    {
+        /// <summary>
+        /// Notifies all connected clients that an user state has changed.
+        /// </summary>
+        /// <param name="userId">The user ID.</param>
+        /// <param name="state">The new <see cref="UserState"/>.</param>
+        void NotifyUserStateChange(int userId, UserState state);
+
+        /// <summary>
+        /// Notifies a new incoming <see cref="Message"/>.
+        /// </summary>
+        /// <param name="message">The <see cref="Message"/>.</param>
+        void NotifyNewMessage(Message message);
+    }
+}
---
+++ MyChat.Service/NotificationManager/NotificationManager.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConnectionManager.cs">
+//    Copyright (c) 2018. All Rights reserved.
+// </copyright>
+// <summary>
+//    This is an implementation of the <see cref="IConnectionManager"/>.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MyChat.Service.ConnectionManager
+{
+    using System.Net;
+    using MyChat.Service.Logging;
+
+    /// <summary>
+    /// This is an implementation of the <see cref="IConnectionManager"/>.
+    /// </summary>
+    internal sealed class ConnectionManager : IConnectionManager
+    {
+        /// <summary> The <see cref="ILogger"/> instance. </summary>
+        private readonly ILogger logger;
+
+        /// <summary> The <see cref="HttpListener"/> instance. </summary>
+        private readonly HttpListener listener = new HttpListener();
+
+        public ConnectionManager(ILogger logger)
+        { }
+    }
+}
